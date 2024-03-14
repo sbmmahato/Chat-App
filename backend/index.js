65014a5798cf8565261897cb3c8364940251c481
Subham Mahato <sbmmahato@gmail.com>
2024-03-14 22:39:21 +0530

    .

--- backend/index.js
+++ backend/index.js
@@ -17,6 +17,12 @@ app.use(express.json());
 //     methods:["GET","POST","PUT","DELETE"]
 // }))
 
+app.use(cors({
+    origin:"*",
+    credentials:true,
+    methods:["GET","POST","PUT","DELETE"]
+}));
+
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Credentials', true);
     res.setHeader('Access-Control-Allow-Origin', '*');
@@ -28,11 +34,7 @@ app.use((req, res, next) => {
     next();
   });
 
-app.use(cors({
-    origin:"*",
-    credentials:true,
-    methods:["GET","POST","PUT","DELETE"]
-}));
+
 
 
 
