cf4b76767aa9b7b1a396556f733f44e5f56647db
Subham Mahato <sbmmahato@gmail.com>
2024-03-20 13:19:07 +0530

    .

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const jwt = require('jsonwebtoken');
 const mongoose = require('mongoose');
 const app = express();
-
+require("dotenv").config();
 
 // const PORT = 4000;
 const SECRET = 'abcd1589qwerty';
